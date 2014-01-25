280e1f7a8e5aa78488a9b2febdb0c32f9c705809
adrian.bala <adrian.bala@gmail.com>
2014-01-25 19:22:04 +0000

    Initial Commit and Configuration

--- chem-calc/src/pl/edu/pjwstk/chemcalc/code/ast/terminal/IDoubleTerminal.java
+++
@@ -1,5 +0,0 @@
-package pl.edu.pjwstk.chemcalc.code.ast.terminal;
-
-public interface IDoubleTerminal extends ITerminalExpression<Double> {
-    public Double getValue();
-}
--- chem-calc/src/pl/edu/pjwstk/chemcalc/code/result/ICompoundResult.java
+++
@@ -1,4 +0,0 @@
-package pl.edu.pjwstk.chemcalc.code.result;
-
-public interface ICompoundResult extends ISimpleResult<CompoundResult> {
-}
